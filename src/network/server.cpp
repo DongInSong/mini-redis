cc6d9388d728834d12e952ad6f3510f1a29c8dfc
Donnie <songdongin97@gmail.com>
2025-07-11 19:26:58 +0900

    [refactor] seperate session from server class

--- src/network/server.cpp
+++ src/network/server.cpp
@@ -1,6 +1,5 @@
 ﻿#include "network/server.hpp"
-#include "command/handler.hpp"
-#include "protocol/parser.hpp"
+#include "network/session.hpp"
 #include <iostream>
 #include <thread>
 #include <memory>
@@ -8,64 +7,6 @@
 
 namespace mini_redis
 {
-  // 세션 클래스 나중에 분리
-  class session : public std::enable_shared_from_this<session>
-  {
-    boost::asio::ip::tcp::socket socket_;
-    boost::asio::streambuf read_buffer_;
-    parser parser_;
-    command_handler handler_;
-
-  public:
-    session(boost::asio::ip::tcp::socket socket, std::shared_ptr<store> s) 
-      : socket_(std::move(socket)), handler_(s) {}
-
-    // 클라이언트 읽기
-    void start() {
-      do_read();
-    }
-
-  private:
-    void do_read() {
-        auto self = shared_from_this();
-        // 비동기적 데이터 읽기
-        socket_.async_read_some(read_buffer_.prepare(1024),
-            [this, self](const boost::system::error_code& ec, std::size_t bytes_transferred) {
-                if (!ec) {
-                    read_buffer_.commit(bytes_transferred);
-                    // streambuf -> string 데이터 변환
-                    std::string data(boost::asio::buffers_begin(read_buffer_.data()),
-                                     boost::asio::buffers_begin(read_buffer_.data()) + bytes_transferred);
-                    read_buffer_.consume(bytes_transferred);
-
-                    // resp 명령어 파싱 및 실행
-                    auto commands = parser_.parse(data);
-                    for (const auto& cmd : commands) {
-                        std::string result = handler_.execute_command(cmd);
-                        std::cout << "Command executed: " << cmd[0] << std::endl;
-                        std::cout << "Response: " << result << std::endl;
-                        do_write(result);
-                    }
-                    // 읽기
-                    do_read();
-                } else if (ec != boost::asio::error::eof) {
-                    std::cerr << "Read error: " << ec.message() << std::endl;
-                }
-            });
-    }
-
-    void do_write(std::string response) {
-      auto self = shared_from_this();
-      auto response_ptr = std::make_shared<std::string>(std::move(response));
-      boost::asio::async_write(socket_, boost::asio::buffer(*response_ptr),
-        [this, self, response_ptr](const boost::system::error_code& ec, std::size_t) {
-          if (ec) {
-            std::cerr << "Write error: " << ec.message() << std::endl;
-          }
-        });
-    }
-  };
-
   server::server(short port)
       : acceptor_(io_context_, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
         store_(std::make_shared<store>())
@@ -124,7 +65,9 @@ namespace mini_redis
     {
       // 데이터 저장소를 생성하고 세션에 전달
       std::make_shared<session>(std::move(socket), store_)->start();
-    } else {
+    }
+    else
+    {
       std::cerr << "Accept error: " << error.message() << std::endl;
     }
     start_accept();
